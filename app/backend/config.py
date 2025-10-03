85af92c519cac39df2946a3995d060564765215c
seobi <dlwlstjq410@gmail.com>
2025-10-03 14:10:51 +0900

    fix: db 비밀번호 오류 수정

--- app/backend/config.py
+++ app/backend/config.py
@@ -10,7 +10,7 @@ def init_firebase():
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-DATABASE_URL = "postgresql+psycopg2://seobi:6769@localhost:5432/emotion_letter"
+DATABASE_URL = "postgresql+psycopg2://seobi:1234@localhost:5432/emotion_letter"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
